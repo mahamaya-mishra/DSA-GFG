d153aaa7ac348e05fe288027144e1882f3b1a3f0
mahamaya-mishra <87880693+mahamaya-mishra@users.noreply.github.com>
2024-06-29 02:02:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Search/binary-search.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        int high =n-1;
+        int low= 0;
+        int mid =0;
+       while (low <= high) {
+         mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == k)
+            return mid;
+
+        // If x greater, ignore left half
+        if (arr[mid] < k)
+            low = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            high = mid - 1;
+    }
+
+    // If we reach here, then element was not present
+    return -1;
+}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
